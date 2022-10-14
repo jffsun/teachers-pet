6bb8489b4d1eca645e83a918a5d5d7b5e4b00bf1
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2022-10-13 21:08:53 -0700

    Delete index.js
    commit error

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoute = require('./home-routes.js');
-
-router.use('/', homeRoute);
-router.use('/api', apiRoutes);
-
-module.exports = router;
