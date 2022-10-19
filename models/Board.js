e662105bc1ef5e59ccbf1f1876440f54b84b42e1
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-10-18 19:13:42 -0700

    Board model, Board data seeds edited

---
+++ models/Board.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Board extends Model {}
+
+Board.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    where: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    when: {
+      // DATETIME - format: YYYY-MM-DD HH:MI:SS
+      type: DataTypes.DATE, 
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'board',
+  }
+);
+
+module.exports = Board;
+
