51dfe111a2fff7175aabcdfb47731b2f89d3501e
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-10-19 13:28:01 -0700

    more naming conventions

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const login = require('./loginRoute.js')
+const loginRoute = require('./loginRoute.js')
 const apiRoutes = require('./api');
 
 router.get('/', (req, res) => {
     res.render('login');
 });
-router.use('/login', login);
+router.use('/login', loginRoute);
 router.use('/api', apiRoutes);
 
 module.exports = router;
