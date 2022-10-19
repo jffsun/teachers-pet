2897c4a96fb3a171086becaa3c20d139bd81c69d
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-10-18 23:31:40 -0700

    POST and GET Routes for announcement board

--- controllers/api/student-info.js
+++ controllers/api/student-info.js
@@ -1,50 +1,54 @@
 const router = require("express").Router();
-const { Student, Parent } = require("../../models");
+const { Student, Parent, Board } = require("../../models");
 const auth = require('../../utils/auth'); 
+const sequelize = require('../../config/connection');
 
-// parents viewing student's data
-// '/api/student-info' endpoint
-router.get("/", auth, (req, res) => {
-    
-    // find all children
-    Student.findAll({
-        include: [Parent],
-            // attributes: ["id", "first_name", "last_name", "allergies", "medication", "diet", "dob", "school_id", "notes", "teacher_id"],
-    })
-    .then(allStudentInfoData => {
-        if(!allStudentInfoData) {
-            res.status(404).json({ message: "No student info"})
-            return;
-        }
-        res.json(allStudentInfoData);
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err)
-    });
-});
+// get all announcements from annoucement board
+// router.get("/", auth, (req, res) => {
 
-router.get("/:school_id", auth, (req, res) => {
+// router.get("/:school_id", auth, (req, res) => {
+router.get("/:school_id", (req, res) => {
+
+    // get all announcements from announcement board
+    const getAllAnnouncements = Board.findAll();
+
     // locates one child by its id value
-    // includes children by attributes
-
-    Student.findOne({
+    const getStudentInfo =  Student.findOne({
         where: {
-            school_id: req.params.school_id,
-        },
-    })
-    .then(studentInfoData => {
-        if (!studentInfoData) {
+            school_id: req.params.school_id
+        }
+    });
+
+   Promise
+    .allSettled([getAllAnnouncements, getStudentInfo])
+    .then(infoAndAnnouncements => {
+        if (!infoAndAnnouncements) {
             res.status(404).json({ message: "No child found"});
             return;
         }
-        res.json(studentInfoData);
+        res.json(infoAndAnnouncements);
     })
     .catch(err => {
         console.log(err)
         res.status(500).json(err)
     });
 });
+    // Student.findOne({
+    //     where: {
+    //         school_id: req.params.school_id,
+    //     },
+    // })
+    // .then(studentInfoData => {
+    //     if (!studentInfoData) {
+    //         res.status(404).json({ message: "No child found"});
+    //         return;
+    //     }
+    //     res.json(studentInfoData);
+    // })
+    // .catch(err => {
+    //     console.log(err)
+    //     res.status(500).json(err)
+    // });
 
 // update a child by its id value
 router.put("/:id", (req, res) => {
@@ -66,25 +70,25 @@ router.put("/:id", (req, res) => {
         });
 });
 
-router.delete("/:id", (req, res) => {
-    // delete a child by its id value
-    parentCard.destroy({
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then(dbparentCardData => {
-            if (!dbparentCardData) {
-                res.status(404).json({ message: "no child found with that id"});
-                return;
-            }
-            res.json(dbparentCardData);
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        });
-});
+// router.delete("/:id", (req, res) => {
+//     // delete a child by its id value
+//     parentCard.destroy({
+//         where: {
+//             id: req.params.id,
+//         },
+//     })
+//         .then(dbparentCardData => {
+//             if (!dbparentCardData) {
+//                 res.status(404).json({ message: "no child found with that id"});
+//                 return;
+//             }
+//             res.json(dbparentCardData);
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         });
+// });
 
 // logout, delete session.
 router.post('/', (req, res) => {
--- controllers/api/teacher-route.js
+++ controllers/api/teacher-route.js
@@ -1,101 +1,108 @@
 const router = require("express").Router();
-const { teacherRoute, parentCard } = require("../../models");
-const auth = require('../../utils/auth'); 
+const { Board } = require("../../models");
 
-// post request announcement board
-// router.post('/', (req, res) => {
-//     // req.body should look like this...
-//     {   
-//         title: "Science Fair"
-//         message: "",
-//         message: "We are going to the San Diego Zoo! Wear comfortable shoes and pack a lunch.",
-//         where: "San Diego Zoo",
-//         when: 11/15/22,
-//     }
-// teacherRoute.create({
-//     include: [announcements],
-// })
-// .then((announcements) => res.status(200).json(announcements))
-// .catch((err) => {
-//     console.log(err);
-//     res.status(400).json(err);
+function formatDate (date) {
+    new Date('"' + date + '"').toISOString().slice(0, 19).replace('T', ' ')
+}
+// parents viewing student's data
+// '/api/student-info' endpoint
+// router.get("/", auth, (req, res) => {
+    
+//     // find all children
+//     Student.findAll({
+//         include: [Parent],
+//             // attributes: ["id", "first_name", "last_name", "allergies", "medication", "diet", "dob", "school_id", "notes", "teacher_id"],
+//     })
+//     .then(allStudentInfoData => {
+//         if(!allStudentInfoData) {
+//             res.status(404).json({ message: "No student info"})
+//             return;
+//         }
+//         res.json(allStudentInfoData);
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         res.status(500).json(err)
+//     });
 // });
 
+// // teacher route data 
+// router.get("/", (req, res) => {
+//     // find all teachers
+//     teacherRoute.findAll({
+//         attributes: ["id", "first_name", "last_name", "dob", "school_id"],
 
-router.get("/", (req, res) => {
-    // find all teachers
-    teacherRoute.findAll({
-        attributes: ["id", "first_name", "last_name", "dob", "school_id"],
+//     })
+//     .then(dbteacherRoute => res.json (dbteacherRoute))
+//     .catch(err => {
+//         console.log(err)
+//         res.status(500).json(err);
+//     });
+// });
 
-    })
-    .then(dbteacherRoute => res.json (dbteacherRoute))
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err);
-    });
-});
+// // gets one individual teacher
+// router.get("/:id", (req, res) => {
+//     // find a techer user by its id
+//     teacherRoute.findOne({
+//         where: {
+//             id:req.params.id,
+//         },
+//         attributes: ["id", "first_name", "last_name", "school_id"],
+//     })
+//     .then(dbteacherRoute => {
+//         if(!dbteacherRoute) {
+//             res.status(404).json({ message: "no teacher found with this id"});
+//             return;
+//         }
+//         res.json(dbUserData);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
-// gets one individual teacher
-router.get("/:id", (req, res) => {
-    // find a techer user by its id
-    teacherRoute.findOne({
-        where: {
-            id:req.params.id,
-        },
-        attributes: ["id", "first_name", "last_name", "school_id"],
+// post request announcement board
+router.post('/', (req, res) => {
+
+    // parse new board annoucement request body 
+    Board.create({
+        title: req.body.title,
+        message: req.body.message,
+        where: req.body.where,
+
+        // format date for sequelize
+        when: new Date('"'+req.body.when+'"').toISOString().slice(0, 19).replace('T', ' ')
     })
-    .then(dbteacherRoute => {
-        if(!dbteacherRoute) {
-            res.status(404).json({ message: "no teacher found with this id"});
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
+
+    .then((newAnnouncement) => res.status(200).json(newAnnouncement))
+    .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
+        res.status(400).json(err);
     });
-});
-// add a teacher
-router.post("/", (req, res) => {
-    teacherRoute.create({
-        id: req.body.id,
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    .then(dbteacherRoute => {
-        if(!dbteacherRoute) {
-            res.status(404).json({ message: "please enter all information for new teacher" })
-            return;
-        }
-        res.json(dbteacherRoute);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
     });
-});
-// update teacher 
-router.put("/:id", (req, res) => {
-    // update a teacher by its id value
-    teacherRoute.update(req.body, {
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then(dbteacherRoute => {
-            if (!dbteacherRoute) {
-                res.status(404).json({ message: "no teacher found with this id"});
-                return;
-            }
-            res.json(dbteacherRoute);
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        });
-});
+
+// // update teacher 
+// router.put("/:id", (req, res) => {
+//     // update a teacher by its id value
+//     teacherRoute.update(req.body, {
+//         where: {
+//             id: req.params.id,
+//         },
+//     })
+//         .then(dbteacherRoute => {
+//             if (!dbteacherRoute) {
+//                 res.status(404).json({ message: "no teacher found with this id"});
+//                 return;
+//             }
+//             res.json(dbteacherRoute);
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         });
+// });
+
 // delete teacher
 router.delete("/:id", (req, res) => {
     // delete teacher by its id value
@@ -115,17 +122,6 @@ router.delete("/:id", (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
-});
-// logout, delete session.
-router.post('/', (req, res) => {
-    if (req.session.logged_in) {
-      // Remove the session variables
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
 });
 
 module.exports = router;
