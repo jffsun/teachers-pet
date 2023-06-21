583062e47b42afbc8cad966ca6893fac9da191ac
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2023-06-21 15:21:09 -0700

    Separate local credentials and deployed credentials for Azure

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,37 @@
 const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
+
 let sequelize;
 
-sequelize = new Sequelize(
-
-  // Environment variables defined in Azure App portal
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: 3306,
-    dialectOptions: {
-      ssl: process.env.AZURE_MYSQL_SSL
+if (process.env.WEBSITE_SITE_NAME) {
+  // Running in Azure environment
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        ssl: {
+          ca: process.env.DB_SSL_CA || '',
+        },
+      },
     }
-  }
-);
+  );
+} else {
+  // Running locally
+  sequelize = new Sequelize(
+    process.env.DB_NAME_LOCAL,
+    process.env.DB_USER_LOCAL,
+    process.env.DB_PASSWORD_LOCAL,
+    {
+      host: process.env.DB_HOST_LOCAL,
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
