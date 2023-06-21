a979e3e4f02f4bccf00b0a9d463a36a6bc411cea
Jeffrey Sun <97923411+jffsun@users.noreply.github.com>
2023-06-21 14:37:22 -0700

    Connection.js to reference new environment variables in Azure portal

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  // Azure provides its own database service, so you don't need to configure JawsDB for deployment
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST, // Update this with the Azure database host
-      dialect: 'mysql',
-      port: 3306
+sequelize = new Sequelize(
+
+  // Environment variables defined in Azure App portal
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306,
+    dialectOptions: {
+      ssl: process.env.AZURE_MYSQL_SSL
     }
-  );
-}
+  }
+);
 
 module.exports = sequelize;
